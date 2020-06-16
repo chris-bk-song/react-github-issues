2625ca20b2ea7b2115eb976d18099a0644eba552
Chris Song <chris.bk.song@gmail.com>
2020-06-16 16:06:16 -0400

    Adds update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import IssuesList from './components/IssuesList';
+import IssueDetail from './components/IssueDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={IssuesList} />
+          <Route path="/issue/:issueNumber" component={IssueDetail} />
+          <Route>
+            <h1>Error! 404 - Not Found</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
